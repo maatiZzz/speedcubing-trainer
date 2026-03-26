2ffc109390c2e0a0b351448df059fef98071c123
matiZzz <m.zbiciak04@gmail.com>
2026-03-26 19:31:15 +0100

    working timer resuming

--- src/app/logic/timer.py
+++ src/app/logic/timer.py
@@ -3,27 +3,30 @@ import customtkinter as ctk
 
 class Timer(ctk.CTkLabel):
     def __init__(self, timer_frame):
-        super().__init__(timer_frame, text = "0.000", font=('Arial', 26))
+        super().__init__(timer_frame, text = "0.000 s", font=('Arial', 26))
         self.grid(row = 0, column = 0, padx = 20, pady = 40, sticky = "new", columnspan = 2)
 
-        self.start_timestamp = 0
+        self.previous_timestamp = 0
         self.current_timestamp = 0
-        self.elapsed_in_ms = 0
+        self.elapsed_in_s = 0
         self.string = ""
         self.running = False
+        self.paused = False
 
     def start_timer(self):
         if self.running == False:
             return
         
-        self.start_timestamp = time.time_ns()
+        self.previous_timestamp = time.time_ns()
         self.update_timer()
 
     def __get_current_time(self):
         self.current_timestamp = time.time_ns()
-        self.elapsed_in_ms =  (self.current_timestamp - self.start_timestamp) / 1000000
-        self.string = str(format(self.elapsed_in_ms, '.3f'))
-        return self.string
+        self.elapsed_in_s += (self.current_timestamp - self.previous_timestamp) / 1000000000
+        self.previous_timestamp = self.current_timestamp
+
+        self.string = str(format(self.elapsed_in_s, '.3f'))
+        return f"{self.string} s"
     
     def change_timer_state(self):
         self.running = not self.running
@@ -39,8 +42,22 @@ class Timer(ctk.CTkLabel):
         return self.running
     
     def reset_timer(self):
-        self.start_timestamp = 0
+        self.previous_timestamp = 0
         self.current_timestamp = 0
-        self.elapsed_in_ms = 0
+        self.elapsed_in_s = 0
 
-        self.configure(text =  "0.000")
+        self.paused = False
+        self.running = False
+
+        self.configure(text =  "0.000 s")
+    
+    def resume(self):
+        self.running = True
+
+        self.start_timer()
+
+    def get_paused(self):
+        return self.paused
+
+    def set_paused(self, state):
+        self.paused = state
--- src/app/view/window.py
+++ src/app/view/window.py
@@ -21,12 +21,65 @@ class App(ctk.CTk):
 
 
         self.cube = Cube()
-        
+
         self.grid_columnconfigure((0), weight = 1)
         self.grid_rowconfigure((0, 1), weight = 1)
 
         # SCRAMBLE FRAME
+        self.__init_scramble_frame()
 
+        # TIMER FRAME
+        self.__init_timer_frame()
+
+
+    def generate(self):
+        self.cube.generate_sequence()
+        sq = self.cube.get_sequence_str()
+        self.sequence_string.configure(text=sq)
+
+    def start_stop_clicked(self):
+        self.timer.change_timer_state()
+        self.timer.start_timer()
+
+        if (self.timer.get_timer_state()):
+            # running
+            self.start_stop_button.configure(text = "Stop", fg_color = "red", hover_color="#7B0000")
+        else:
+            # paused
+            self.start_stop_button.grid_forget()
+            self.resume_button.grid(row = 1, column = 0, padx = 20, pady = 40, sticky = "sew")
+            self.timer.set_paused(True)
+
+    def resume_button_clicked(self):
+        if self.timer.get_paused():
+            # allow to resume if paused previosly
+            self.resume_button.grid_forget()
+            self.start_stop_button.grid(row = 1, column = 0, padx = 20, pady = 40, sticky = "sew")
+            
+            self.timer.resume()
+        
+    def reset_clicked(self):
+        self.timer.reset_timer()
+        self.resume_button.grid_forget()
+        self.start_stop_button.grid(row = 1, column = 0, padx = 20, pady = 40, sticky = "sew")
+        self.start_stop_button.configure(text = "Start", fg_color = "green", hover_color="#006810")
+
+    def __init_timer_frame(self):
+        self.timer_frame = ctk.CTkFrame(self)
+        self.timer_frame.grid(row=1, column=0, padx=20, pady=20, sticky="sew")
+        self.timer_frame.grid_columnconfigure((0, 1), weight=1)
+        
+        self.timer = Timer(self.timer_frame)
+        self.start_stop_button = ctk.CTkButton(self.timer_frame, text="Start", command=self.start_stop_clicked,
+                                                bg_color="black", fg_color="green", hover_color="#006810", font=('Arial', 18))
+        self.resume_button = ctk.CTkButton(self.timer_frame, text="Resume", command=self.resume_button_clicked,
+                                                bg_color="black", fg_color="green", hover_color="#006810", font=('Arial', 18))
+        self.reset_button = ctk.CTkButton(self.timer_frame, text="Reset", command=self.reset_clicked,
+                                        bg_color="black", fg_color="#ff4d01", hover_color="#a63400", font=('Arial', 18))
+        self.start_stop_button.grid(row = 1, column = 0, padx = 20, pady = 40, sticky = "sew")
+        self.reset_button.grid(row = 1, column = 1, padx = 20, pady = 40, sticky = "sew")
+
+    def __init_scramble_frame(self):
         self.sequence_frame = ctk.CTkFrame(self)
         self.sequence_frame.grid(row=0, column=0, padx=20, pady=20, sticky="new")
         self.sequence_frame.grid_columnconfigure((0), weight=1)
@@ -36,34 +89,3 @@ class App(ctk.CTk):
                                                bg_color="black", font=('Arial', 18))
         self.sequence_string.grid(row = 0, column = 0, padx = 20, pady = 40, sticky = "new")
         self.generate_button.grid(row = 1, column = 0, padx = 20, pady = 40, sticky = "sew")
-
-        # TIMER FRAME
-
-        self.timer_frame = ctk.CTkFrame(self)
-        self.timer_frame.grid(row=1, column=0, padx=20, pady=20, sticky="sew")
-        self.timer_frame.grid_columnconfigure((0, 1), weight=1)
-        
-        self.timer = Timer(self.timer_frame)
-        self.start_stop_button = ctk.CTkButton(self.timer_frame, text="Start", command=self.timer_clicked,
-                                                bg_color="black", fg_color="green", font=('Arial', 18))
-        self.reset_button = ctk.CTkButton(self.timer_frame, text="Reset", command=self.reset_clicked,
-                                           bg_color="black", fg_color="#ff4d01", font=('Arial', 18))
-        self.start_stop_button.grid(row = 1, column = 0, padx = 20, pady = 40, sticky = "sew")
-        self.reset_button.grid(row = 1, column = 1, padx = 20, pady = 40, sticky = "sew")
-
-    def generate(self):
-        self.cube.generate_sequence()
-        sq = self.cube.get_sequence_str()
-        self.sequence_string.configure(text=sq)
-
-    def timer_clicked(self):
-        self.timer.change_timer_state()
-        self.timer.start_timer()
-
-        if (self.timer.get_timer_state()):
-            self.start_stop_button.configure(text = "Stop", fg_color = "red")
-        else:
-            self.start_stop_button.configure(text = "Start", fg_color = "green")
-
-    def reset_clicked(self):
-        self.timer.reset_timer()
